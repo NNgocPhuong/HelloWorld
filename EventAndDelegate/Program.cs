e5a63fc3135359cca222857eef69a0bbdf32523b
Phuong.NN203535@sis.hust.edu.vn <Phuong.NN203535@sis.hust.edu.vn>
2023-03-23 22:14:26 +0700

    bản cuối

--- EventAndDelegate/Program.cs
+++ EventAndDelegate/Program.cs
@@ -7,57 +7,78 @@ namespace CsEvent
      *  subscriber - class : nhận sự kiện 
      */
     public delegate void suKienNhapSo(int x);
-    class UserInput
+
+    class DuLieuNhap : EventArgs
     {
-        public suKienNhapSo su_kien_nhap_so { set; get; }
-        public void Input()
+        public int data { set; get; }
+        public DuLieuNhap(int x)
         {
-            do
+            data = x;
+        }
+        class UserInput
+        {
+            //public event suKienNhapSo su_kien_nhap_so;
+            public event EventHandler su_kien_nhap_so;
+            // ~ delegate void Kieu(object sender, EventArgs args)
+            public void Input()
             {
+
                 Console.Write("Nhap vao so nguyen: ");
                 string s = Console.ReadLine();
                 int i = Int32.Parse(s);
-                su_kien_nhap_so?.Invoke(i);
+                su_kien_nhap_so?.Invoke(this, new DuLieuNhap(i));
+
             }
-            while (true);
-        } 
-    }
-    // tính căn...
-    class TinhCan
-    {
-        public void Sub(UserInput input)
-        {
-            input.su_kien_nhap_so = Can;
         }
-        public void Can(int a)
+        // tính căn...
+        // ~ delegate void Kieu(object sender, EventArgs args)
+        class TinhCan
         {
-            Console.WriteLine($"can ban 2 cua so {a} la: {Math.Sqrt(a)}");
+            public void Sub(UserInput input)
+            {
+                input.su_kien_nhap_so += Can;
+            }
+            public void Can(object sender, EventArgs e)
+            {
+                DuLieuNhap duLieuNhap = (DuLieuNhap)e;
+                int a = duLieuNhap.data;
+                Console.WriteLine($"can ban 2 cua so {a} la: {Math.Sqrt(a)}");
 
+            }
         }
-    }
-    class BinhPhuong
-    {
-        public void Sub(UserInput input)
+        class BinhPhuong
         {
-            input.su_kien_nhap_so = binh_phuong;
+            public void Sub(UserInput input)
+            {
+                input.su_kien_nhap_so += binh_phuong;
+            }
+            public void binh_phuong(object sender, EventArgs e)
+            {
+                DuLieuNhap duLieu = (DuLieuNhap)e;
+                int a = duLieu.data;
+                Console.WriteLine($"Binh phuong cua so {a} la: {a * a}");
+            }
         }
-        public void binh_phuong(int a)
+        class Program
         {
-            Console.WriteLine($"Binh phuong cua so {a} la: {a * a}");
-        }
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            UserInput user_input = new UserInput();
-            
-            TinhCan tinhCan = new TinhCan();
-            tinhCan.Sub(user_input);
+            static void Main(string[] args)
+            {
+                UserInput user_input = new UserInput();
 
-            BinhPhuong bin = new BinhPhuong();
-            bin.Sub(user_input);
-            user_input.Input();
+                user_input.su_kien_nhap_so += (sender, e) =>
+                {
+                    DuLieuNhap duLieuNhap = (DuLieuNhap)e;
+                    Console.WriteLine("Ban vua nhap so: " + duLieuNhap.data);
+                };
+
+                TinhCan tinhCan = new TinhCan();
+                tinhCan.Sub(user_input);
+
+                BinhPhuong bin = new BinhPhuong();
+                bin.Sub(user_input);
+
+                user_input.Input();
+            }
         }
     }
 }
