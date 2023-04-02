49fea0b8baf007a8a56a0231fe39d44c986ed964
Phuong.NN203535@sis.hust.edu.vn <Phuong.NN203535@sis.hust.edu.vn>
2023-04-02 15:13:10 +0700

    them Exception

---
+++ Exception/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b;
+            string s = Console.ReadLine();
+            b = int.Parse(s);
+
+            try
+            {
+                int c = a / b;
+                Console.WriteLine(c);
+            }
+            catch 
+            {
+                Console.WriteLine("Loi khi thuc hien phep chia");
+            }
+            Console.WriteLine("Phuong dep trai");
+        }
+    }
+}
