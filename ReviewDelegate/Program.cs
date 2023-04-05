5bbd73a3ecf2cdbcb4ed5005cec4667cbd46bae1
Phuong.NN203535@sis.hust.edu.vn <Phuong.NN203535@sis.hust.edu.vn>
2023-04-05 12:26:37 +0700

    reviewDelegate

---
+++ ReviewDelegate/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using ReviewDelegate;
+
+namespace ReviewDelegate
+{
+    internal class Program
+    {
+        //public Action<int> action;
+        public static void tinhTong()
+        {
+            TestAction(4, 5, (x) =>
+                Console.WriteLine($"Tong hai so la: {x}"));
+        }
+        
+        static void Main(string[] args)
+        {
+
+            //c = TestFunc.Sum(a, b);
+            UseFunc();
+            tinhTong();
+            Console.ReadLine();
+            
+        }
+        public static void UseFunc()
+        {
+            TestFunc a = new TestFunc(3, 2);
+            TestFunc b = new TestFunc(1, 2);
+            
+            Func<TestFunc, TestFunc, TestFunc> func;
+            func = TestFunc.Sum;
+            var c = func(a, b);
+            c.Show();
+        }
+        public static void TestAction(int a, int b , Action<string> callBack)
+        {
+            int c = a + b;
+            callBack(c.ToString());
+        }
+        
+    }
+}
---
+++ ReviewDelegate/TestFunc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+// Func<kieuBien1, kieuBien2,..., kieuTraVe> ten;
+namespace ReviewDelegate
+{
+    internal class TestFunc
+    {
+        public int tu_so;
+        public int TuSo
+        {
+            get { return tu_so; }
+            set { tu_so = value; }
+        }
+        public int mau_so;
+        public int MauSo
+        {
+            set { mau_so = value; }
+            get { return mau_so; }
+        }
+        public TestFunc(int tu = 0, int mau = 0)
+        {
+            this.tu_so = tu;
+            this.mau_so = mau;
+        }
+        public static TestFunc operator + (TestFunc a, TestFunc b)
+        {
+            TestFunc testFunc = new TestFunc();
+            testFunc.tu_so = a.tu_so * b.mau_so + b.tu_so * a.mau_so;
+            testFunc.mau_so = a.mau_so * b.mau_so;
+            return testFunc;
+        }
+        public static TestFunc Sum(TestFunc a, TestFunc b)
+        {
+            return a + b;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Tu so = {this.tu_so}, Mau so = {this.mau_so}");
+        }
+    }
+}
